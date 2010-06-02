8e5b808e45c64abe2278659a1e7f05b5a318d6c3
Souheil CHELFOUH <trollfot@gmail.com>
2010-06-02 22:29:56 +0000

    back to dev

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ tests_require = [
 
 setup(
     name='grokcore.view',
-    version='1.13.5',
+    version='1.13.6dev',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
